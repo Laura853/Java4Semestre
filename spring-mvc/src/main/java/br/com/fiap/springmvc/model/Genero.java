11ad04bd6755ecb0c27c1382c0fe51b29cb8ce8c
laura853 <lauralopess2006@gmail.com>
2026-08-07 22:36:40 -0300

    criamos estrutura(model,controller, repository e template) e fizemos um 'Hello World' localhost

---
+++ spring-mvc/src/main/java/br/com/fiap/springmvc/model/Genero.java
@@ -0,0 +1,13 @@
+package br.com.fiap.springmvc.model;
+
+public enum Genero {
+    TERROR,
+    ROMANCE,
+    ACAO,
+    SUSPENSE,
+    FICCSO_CIENTIFICA,
+    BIOGRAFIA,
+    FILOSOFIA,
+    HISTORIA,
+    CIENCIA
+}
